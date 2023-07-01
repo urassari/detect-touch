3519074815a8d39e52dad8091d364dbe6e649c1b
urassari <136824813+urassari@users.noreply.github.com>
2023-07-01 23:02:06 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,30 @@
+//% color="#FFAB19"
 namespace DetectPins {
 
 
 //% block
-export function randomBoolean(): boolean {
-    return false;
+export function leftTouched(): boolean {
+    return pins.analogReadPin(AnalogPin.P0) > 970 ;
 }
 
+//% block
+export function leftMiddleTouched(): boolean {
+    return pins.analogReadPin(AnalogPin.P4) > 940;
+}
 
+//% block
+export function middleTouched(): boolean {
+    return pins.analogReadPin(AnalogPin.P1) > 910;
+}
+
+//% block
+export function middleRightTouched(): boolean {
+    return pins.analogReadPin(AnalogPin.P10) > 910;
+}
+
+//% block
+export function rightTouched(): boolean {
+    return pins.analogReadPin(AnalogPin.P2) > 850;
+}
+    
 }
